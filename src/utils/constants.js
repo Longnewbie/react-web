ae5c7050b93f5a9891548a15499c9b5fac2e347e
lygialong <lygialong201@gmail.com>
2024-10-20 00:05:51 +0700

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://node2fa-api.onrender.com'
+}
+
+export const API_ROOT = apiRoot
+// export const API_ROOT = 'http://localhost:8017'
 // export const API_ROOT = 'https://node2fa-api.onrender.com'
