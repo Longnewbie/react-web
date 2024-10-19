3f6616c1c68e10990e65ad079602c9574ef374ae
lygialong <lygialong201@gmail.com>
2024-10-19 23:26:32 +0700

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://node2fa-api.onrender.com'
+export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'https://node2fa-api.onrender.com'
